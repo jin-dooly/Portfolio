1d3a31989e3ed9f19f5f3cfa181a095853b86ed9
LeeJinIe <jj074567@gmail.com>
2024-06-20 04:52:44 +0900

    Fix: 빈페이지 출력 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
